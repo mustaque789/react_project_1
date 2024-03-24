f8c9fd7a4a8faf528d48688a51d129eeb901cbe2
Mustaque Khan <130723242+mustaque789@users.noreply.github.com>
2024-03-24 03:54:52 -0400

    c

--- src/App.js
+++ src/App.js
@@ -4,10 +4,33 @@ const App = ()=>{
 return(
   <>
   <div>
+    <DemoComp/>
+    <DemoComp2/>
   <h1>Hello world! this is my first project</h1>
+  <DemoComp/>
   </div>
   </>
 )
+}
+
+const DemoComp2 = ()=>{
+  return(
+    <>
+     
+    <h2>Mustaque</h2>
+  
+    </>
+  )
+}
+
+const DemoComp = ()=>{
+  return(
+    <>
+   
+    <h2>Sahil</h2>
+    
+    </>
+  )
 }
 
 export default App
